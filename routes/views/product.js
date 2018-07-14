292aa993a968cb9cece4b6ef3cadb4e4918db6f9
nmasuki <masuki@geopoll.com>
2018-07-14 11:02:48 +0300

    Update..

--- routes/views/product.js
+++ routes/views/product.js
@@ -3,88 +3,87 @@ var router = keystone.express.Router();
 var ProductRating = keystone.list('ProductRating');
 
 router.get("/:product", function (req, res) {
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
-	var regex = new RegExp(req.params.product.cleanId().trim(), "i");
+    var view = new keystone.View(req, res);
+    var locals = res.locals;
+    var regex = new RegExp(req.params.product.cleanId().trim(), "i");
 
-	locals.page = Object.assign(locals.page, {title: ""});
+    locals.page = Object.assign(locals.page, {title: ""});
 
-	view.on('init', function (next) {
-		keystone.list('Product').findOnePublished({href: regex}, function (err, product) {
-			if (product) {
-				locals.product = product;
-				locals.page.title = product.pageTitle;
+    view.on('init', function (next) {
+        keystone.list('Product').findOnePublished({href: regex}, function (err, product) {
+            if (product) {
+                locals.product = product;
+                locals.page.title = product.pageTitle;
 
-                locals.page.breadcrumbs = locals.page.breadcrumbs || {};
-				[
+                [
                     product.category && product.category.name,
                     product.subCategory && product.subCategory.name,
-					product.name
-                ].filter(i=>!!i).forEach(i=>{
-                	locals.page.breadcrumbs.push({
-						title: i,
-						href: "/" + i.toLowerCase()
-					})
-				});
+                    product.name
+                ].filter(i => !!i).forEach(i => {
+                    locals.breadcrumbs.push({
+                        title: i,
+                        href: "/" + i.toLowerCase()
+                    })
+                });
 
-                locals.page.breadcrumbs.push({
+                locals.breadcrumbs.push({
                     title: product.name,
                     href: "/product/" + product.href
                 });
 
-				locals.page.title = locals.page.title || [
-					product.name,
-					product.category && product.category.name,
-					product.subCategory && product.subCategory.name,
-					product.brand && product.brand.name,
-				].filter(a => !!a).join(" - ") + " | " + keystone.get("name");
+                locals.page.title = locals.page.title || [
+                    product.name,
+                    product.category && product.category.name,
+                    product.subCategory && product.subCategory.name,
+                    product.brand && product.brand.name,
+                ].filter(a => !!a).join(" - ") + " | " + keystone.get("name");
 
-				locals.userRating = product.ratings && product.ratings.find(r => r.userId === req.session.id);
+                locals.userRating = product.ratings && product.ratings.find(r => r.userId === req.session.id);
 
-				product.findSimilar((err, products) => {
-					if (products)
-						locals.similar = products
-							.orderBy(p => Math.abs(p.popularity - product.popularity))
-							.slice(0, 10);
+                product.findSimilar((err, products) => {
+                    if (products)
+                        locals.similar = products
+                            .orderBy(p => Math.abs(p.popularity - product.popularity))
+                            .slice(0, 10);
 
 
-					//popularity goes up
-					product.addPopularity(1);
-					next(err);
-				});
-			} else
-				next(err);
-		});
-	});
+                    //popularity goes up
+                    product.addPopularity(1);
+                    next(err);
+                });
+            } else
+                next(err);
+        });
+    });
 
 // Render View
-	view.render('product');
+    view.render('product');
 });
 
 router.get("/rate/:product/:rating", function (req, res) {
-	keystone.list('Product').findOnePublished({href: req.params.product})
-		.exec(function (err, product) {
-			if (product) {
+    keystone.list('Product').findOnePublished({href: req.params.product})
+        .exec(function (err, product) {
+            if (product) {
 
-				var userRating = new ProductRating.model({
-					userId: req.session.id,
-					product: product,
-					rating: req.params.rating,
-				});
+                var userRating = new ProductRating.model({
+                    userId: req.session.id,
+                    product: product,
+                    rating: req.params.rating,
+                });
 
-				userRating.save((err, a) => {
-					if (err)
-						console.err(err);
+                userRating.save((err, a) => {
+                    if (err)
+                        console.err(err);
 
-					product.ratings.push(userRating);
-					product.save();
-				});
+                    product.ratings.push(userRating);
+                    product.save();
+                });
 
 
-				return res.send({state: true, rating: userRating})
-			}
-			next(err);
-		});
+                return res.send({state: true, rating: userRating})
+            }
+            next(err);
+        });
 });
 
 exports = module.exports = router;
