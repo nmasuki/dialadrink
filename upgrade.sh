ceee3a003aaf2d02bb46034f30bb06efb7f480b2
Nelson <nmasuki@gmail.com>
2019-03-01 00:06:41 +0300

    Server updates

--- upgrade.sh
+++ upgrade.sh
@@ -1,9 +1,12 @@
 cp .env /var/tmp/dialadrink.env
 git stash
+git reset HEAD 
 git pull -X theirs
-git add .
 git stash pop
 
 cp /var/tmp/dialadrink.env .env
 grunt build
+git add .
+git commit -a "Server updates"
+git push
 pm2 reload main && pm2 monit
