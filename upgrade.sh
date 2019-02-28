58a3b0cc571974d9564ac557416fccb0229bf6a4
Nelson <nmasuki@gmail.com>
2019-02-28 19:15:54 +0300

    template.

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,9 @@
 cp .env /var/tmp/dialadrink.env
 git stash
-git pull -X theirs && grunt build && cp /var/tmp/dialadrink.env .env 
-git stash pop  
+git pull -X theirs && grunt build
+
+cp /var/tmp/dialadrink.env .env
+git add .
+git stash pop
 pm2 reload main && pm2 monit
 
