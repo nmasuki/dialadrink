11d5e989ae6417db365956717505b105ce071c54
nmasuki <masuki@geopoll.com>
2019-03-12 17:02:35 +0300

    null check fix on notification

--- public/js/app.js
+++ public/js/app.js
@@ -301,19 +301,22 @@ var app = {
 		});
 	},
 
-	showNotification: function (from, align) {
+	showNotification: function (msg, from, align) {
 		var color = Math.floor((Math.random() * 4) + 1);
-		$.notify({
-			icon: "pe-7s-gift",
-			message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
-		}, {
-			type: app.type[color],
-			timer: 4000,
-			placement: {
-				from: from,
-				align: align
-			}
-		});
+		if (!$.notify)
+			app.showToast(msg)
+		else
+			$.notify({
+				icon: "pe-7s-gift",
+				message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
+			}, {
+				type: app.type[color],
+				timer: 4000,
+				placement: {
+					from: from,
+					align: align
+				}
+			});
 	}
 };
 
