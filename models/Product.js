842e489f4416e733d7def851487fa8f6ecc742d8
nmasuki <masuki@geopoll.com>
2019-05-29 15:41:45 +0300

    npm audit fix..

--- models/Product.js
+++ models/Product.js
@@ -474,8 +474,7 @@ Product.getUIFilters = function(products){
     var strUIfilters = uifilters.filter(f=>f.filter && !/^\d/.test(f.filter))
         .orderBy(f => -f.hits)
         .distinctBy(f => f.filter.trim())
-        .distinctBy(f => f.g.map(p => p.id).orderBy(i => i).join("|"))
-        .map(f => f.filter);
+        .distinctBy(f => f.g.map(p => p.id).orderBy(i => i).join("|"));
 
     strUIfilters.forEach(s => {
         if (l <= 70) {
