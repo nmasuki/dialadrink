7d6b5f3960c3ead0187c8a75294c6817cc34c91c
nmasuki <masuki@geopoll.com>
2019-07-12 10:15:08 +0300

    Instant Checkout desktop

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 47;
+var CACHE_VERSION = 48;
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
 function getCacheName(destination, inc) {
