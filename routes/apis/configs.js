f65c9999b7e58b6a8bc24566e1a8d6632906afbf
nmasuki <masuki@geopoll.com>
2019-08-28 20:40:06 +0300

    API endpoint to register FCM COdes.

--- routes/apis/configs.js
+++ routes/apis/configs.js
@@ -1,22 +1,25 @@
 var keystone = require('keystone');
 var Page = keystone.list("Page");
+var Location = keystone.list("Location");
 
 var router = keystone.express.Router();
 
 router.get('/', function (req, res) {
-	var json = {
-		response: "success",
-		data: [{
-			title: "name",
-			value: keystone.get('name')
-		}]
-	};
+    var json = {
+        response: "success",
+        data: [{
+            title: "name",
+            value: keystone.get('name')
+        }]
+    };
 
-	res.send(json);
+    res.send(json);
 });
 
-router.get("/banners", function(reg, res){
-    var filters = {$where:'this.mobileBannerImages && this.mobileBannerImages.length > 1'};
+router.get("/banners", function (req, res) {
+    var filters = {
+        $where: 'this.mobileBannerImages && this.mobileBannerImages.length > 1'
+    };
 
     Page.model.find(filters)
         .exec((err, pages) => {
@@ -25,15 +28,16 @@ router.get("/banners", function(reg, res){
                     response: "error",
                     message: "Unexpected error while reading mobile banners! " + err
                 });
-            
+
             var json = {
                 response: "error",
                 message: ""
             };
 
-            if(pages && pages.length){
-                var banners = pages.selectMany(p=>{
-                    return p.mobileBannerImages.map(b=>{
+            if (pages && pages.length) {
+                json.response = "success";
+                json.data = pages.selectMany(p => {
+                    return p.mobileBannerImages.map(b => {
                         return {
                             id: p.id,
                             title: p.title,
@@ -43,12 +47,35 @@ router.get("/banners", function(reg, res){
                         };
                     });
                 });
-                json.response = "success";
-                json.data = banners;
             }
 
             res.send(json);
-        });    
+        });
+});
+
+router.get("/locations", function (req, res) {
+    Location.model.find({ show: true })
+        .exec((err, locations) => {
+            if (err)
+                return res.send({
+                    response: "error",
+                    message: "Unexpected error while reading locations! " + err
+                });
+
+            var json = {
+                response: "error",
+                message: ""
+            };
+
+            if (locations && locations.length) {
+                json.response = "success";
+                json.data = locations.map(l=>l.toObject());
+            }else{
+                json.message = "No locations found!";
+            }
+
+            res.send(json);
+        });
 });
 
 exports = module.exports = router;
