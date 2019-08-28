0b969a3af6887a36165b84db4103ca9902de2940
nmasuki <masuki@geopoll.com>
2019-08-28 16:14:05 +0300

    Moved banners api to configs

--- routes/apis/configs.js
+++ routes/apis/configs.js
@@ -1,7 +1,5 @@
 var keystone = require('keystone');
-
-var CartItem = keystone.list("CartItem");
-var Product = keystone.list("Product");
+var Page = keystone.list("Page");
 
 var router = keystone.express.Router();
 
@@ -15,6 +13,42 @@ router.get('/', function (req, res) {
 	};
 
 	res.send(json);
+});
+
+router.get("/banners", function(reg, res){
+    var filters = {$where:'this.mobileBannerImages && this.mobileBannerImages.length > 1'};
+
+    Page.model.find(filters)
+        .exec((err, pages) => {
+            if (err)
+                return res.send({
+                    response: "error",
+                    message: "Unexpected error while reading mobile banners! " + err
+                });
+            
+            var json = {
+                response: "error",
+                message: ""
+            };
+
+            if(pages && pages.length){
+                var banners = pages.selectMany(p=>{
+                    return p.mobileBannerImages.map(b=>{
+                        return {
+                            id: p.id,
+                            title: p.title,
+                            meta: p.meta,
+                            image: b.secure_url,
+                            status: ""
+                        };
+                    });
+                });
+                json.response = "success";
+                json.data = banners;
+            }
+
+            res.send(json);
+        });    
 });
 
 exports = module.exports = router;
--- routes/apis/user.js
+++ routes/apis/user.js
@@ -1,6 +1,5 @@
 var keystone = require('keystone');
 var Client = keystone.list("Client");
-var Page = keystone.list("Page");
 
 var router = keystone.express.Router();
 
@@ -191,42 +190,6 @@ router.post("/login", function (req, res) {
             res.send(json);
         });
 
-});
-
-router.get("/banners", function(reg, res){
-    var filters = {$where:'this.mobileBannerImages && this.mobileBannerImages.length > 1'};
-
-    Page.model.find(filters)
-        .exec((err, pages) => {
-            if (err)
-                return res.send({
-                    response: "error",
-                    message: "Unexpected error while reading mobile banners! " + err
-                });
-            
-            var json = {
-                response: "error",
-                message: ""
-            };
-
-            if(pages && pages.length){
-                var banners = pages.selectMany(p=>{
-                    return p.mobileBannerImages.map(b=>{
-                        return {
-                            id: p.id,
-                            title: p.title,
-                            meta: p.meta,
-                            image: b.secure_url,
-                            status: ""
-                        };
-                    });
-                });
-                json.response = "success";
-                json.data = banners;
-            }
-
-            res.send(json);
-        });    
 });
 
 router.post("/register_fcm", function (req, res) {
