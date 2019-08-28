bbcadcf53ffffc62c5231ef10f8ee28b826e7632
nmasuki <masuki@geopoll.com>
2019-08-28 16:54:58 +0300

    Empty string for default discount

--- models/Product.js
+++ models/Product.js
@@ -220,7 +220,7 @@ Product.schema.methods.toAppObject = function(){
         brand: d.brand ? d.brand.name : null,
         company: d.brand && d.brand.company ? d.brand.company.name : null,
         price: d.price,
-        discount: d.offerPrice? d.price - d.offerPrice: 0,
+        discount: (d.offerPrice? d.price - d.offerPrice: 0) || "",
         currency: d.currency,
         options: d.options
     });
