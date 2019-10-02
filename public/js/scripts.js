c450cae3cc2309efc96fab49659f04bc0cf7b0c6
nmasuki <masuki@geopoll.com>
2019-10-02 16:18:28 +0300

    Voice search

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -282,7 +282,7 @@ function handleSearchingAndPaging() {
             if($(this).val())
                 $("#search").attr("src", "/assets/icon-search.png");
             else
-                $("#search").attr("src", "/assets/icon-voice-search.gif");
+                $("#search").attr("src", "/assets/icon-voice-search.png");
         }
     });
 
