dbd64fbed79338ae33ff6e0f34ae0fd9497503c5
nmasuki <masuki@geopoll.com>
2019-10-23 12:43:19 +0300

    CSS updates

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 49.1;
+var CACHE_VERSION = 49.3;
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
 function getCacheName(destination, inc) {
