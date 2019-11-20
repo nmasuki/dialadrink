0cc337d062fe65e9f8844391ebb1bb750744a4d4
nmasuki <masuki@geopoll.com>
2019-11-20 21:48:39 +0300

    fix bag on format

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 var keystone = require('./app-init');
 
-var MoveSms = require("./helpers/movesms");
-var sms = new MoveSms();
-
-sms.balance().then(console.log).catch(console.error);
-
-
 // Start Keystone to connect to your database and initialise the web server
-//keystone.start();
+keystone.start();
 
 require('./workers');
