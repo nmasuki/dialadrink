36c4161f9d78ae150a7a885d787d8597c16a868f
nmasuki <masuki@geopoll.com>
2019-12-22 15:19:07 +0300

    SW version increment

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 61;
+var CACHE_VERSION = 66;
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
 function getCacheName(destination, inc) {
