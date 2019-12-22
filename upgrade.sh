a7989153da27b430aa1310a529f06c6be4c6f585
Nelson <nmasuki@gmail.com>
2019-12-22 20:54:20 +0300

    Script update

--- upgrade.sh
+++ upgrade.sh
@@ -2,10 +2,11 @@ cp .env /var/tmp/dialadrink.env
 git checkout .
 git pull -X theirs
 
-cp /var/tmp/dialadrink.env .env
 grunt build
 git add .
 git commit -m "Server updates"
 git push
+
+cp /var/tmp/dialadrink.env .env
 pm2 reload main --update-env --log-date-format 'DD-MM HH:mm:ss.SSS'
 pm2 log
