6fb13a1a47099a3019616c9e0b1f6c07c2b9566f
nmasuki <masuki@geopoll.com>
2020-01-04 23:42:06 +0300

    Small fixes on cart Item

--- models/CartItem.js
+++ models/CartItem.js
@@ -95,7 +95,7 @@ CartItem.schema.methods.toAppObject = function () {
         price: priceOption.price,
         offerPrice: priceOption.offerPrice,
         quantity: priceOption.quantity,
-        product: this.product && this.product.toAppObject ? this.product.toAppObject() : null,
+        //product: this.product && this.product.toAppObject ? this.product.toAppObject() : null,
         productId: this.product && this.product.id
     });
 
