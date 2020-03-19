db5a314eb369c673a46e748ff16ce24d8d651239
Nelson <nmasuki@gmail.com>
2020-03-19 20:24:55 +0300

    Server updates

--- upgrade.sh
+++ upgrade.sh
@@ -8,5 +8,5 @@ git commit -m "Server updates"
 git push
 
 cp /var/tmp/dialadrink-prod.env .env
-pm2 reload main --update-env --log-date-format 'DD-MM HH:mm:ss.SSS'
+pm2 reload all --update-env --log-date-format 'DD-MM HH:mm:ss.SSS'
 pm2 log
