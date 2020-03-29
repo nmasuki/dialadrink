ac75ea61bc83578f0465cfa696df85222ea49f2e
Nelson <nmasuki@gmail.com>
2020-03-29 13:43:53 +0300

    Server updates

--- upgrade.sh
+++ upgrade.sh
@@ -1,12 +1,18 @@
 cp .env /var/tmp/dialadrink-prod.env
+cp upgrade.sh /var/tmp/upgrade.sh
+
 git checkout .
 git pull -X theirs
+
+cp /var/tmp/dialadrink-prod.env .env
+cp /var/tmp/upgrade.sh upgrade.sh
 
 grunt build
 git add .
 git commit -m "Server updates"
 git push
 
-cp /var/tmp/dialadrink-prod.env .env
-pm2 reload all --update-env --log-date-format 'DD-MM HH:mm:ss.SSS'
+pm2 reload dev --update-env --log-date-format 'DD-MM HH:mm:ss.SSS'
+pm2 reload main --update-env --log-date-format 'DD-MM HH:mm:ss.SSS'
+
 pm2 log
