4cc5939e0eea03b056bfa40cf9d8fc5e766c50d3
nmasuki <masuki@geopoll.com>
2020-07-25 10:24:01 +0300

    updates

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 90.2;
+var CACHE_VERSION = 90.3;
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
 function getCacheName(destination, inc) {
