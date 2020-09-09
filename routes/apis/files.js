f5278ef4900675c89a322d6bb172866339004db2
nmasuki <masuki@geopoll.com>
2020-09-09 10:53:41 +0300

    Some logs

--- routes/apis/files.js
+++ routes/apis/files.js
@@ -15,8 +15,8 @@ router.post('/upload', function (req, res) {
         for(var i in req.files){
             var file = req.files[i];
             var options =  {public_id: "app-uploads/" + file.originalname };
-            cloudinary.v2.uploader.upload(
-                file.path, options, (error, result) => {
+
+            cloudinary.v2.uploader.upload(file.path, options, (error, result) => {
                     if(error)
                         json.errors.push(error);
                     else{
@@ -24,10 +24,8 @@ router.post('/upload', function (req, res) {
                         ls.save(result);
                     }
                     
-                    var l = json.data.length + json.errors.length;
-                    if(l >= Object.keys(req.files).length){
+                    if(json.data.length + json.errors.length >= Object.keys(req.files).length){
                         json.response = json.data.length? "success": "error";
-                        console.log(json);
                         res.send(json);
                     }
                 });
