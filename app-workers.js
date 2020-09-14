e8db81de43b03222c0ae58a29de2712f37fb1f22
nmasuki <masuki@geopoll.com>
2020-09-14 19:01:52 +0300

    Order number update..

--- app-workers.js
+++ app-workers.js
@@ -2,5 +2,5 @@ var keystone = require('./app-init');
 var workers = require('./workers');
 
 // Start Keystone to connect to your database and initialise the web server
-keystone.start();
+//keystone.start();
 workers.start();
