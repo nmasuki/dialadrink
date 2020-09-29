7447bd043258f3983bccec35538bfafba7362eb8
nmasuki <masuki@geopoll.com>
2020-09-29 13:26:04 +0300

    Remove client 'caching'..

--- updates/0.2.4-extract-clients.js
+++ updates/0.2.4-extract-clients.js
@@ -2,7 +2,7 @@ var keystone = require('keystone');
 var Order = keystone.list('Order');
 
 exports = module.exports = function (done) {
-    console.log("Extracting clients from orders");
+    console.log("Extracting clients from orders...");
     Order.model.find({})
         .sort({orderDate: -1})
         .exec(function (err, orders) {
@@ -10,7 +10,7 @@ exports = module.exports = function (done) {
             orders = orders.filter(o => !o.client || !o.client.firstName);
 
             (function updateClient(){
-                console.log(`Extracting client from order ${index + 1}/${orders.length}`)
+                console.log(`Extracting client from order ${index + 1}/${orders.length}...`);
                 var order = orders[++index];
                 if(order)
                     order.updateClient(updateClient);                
