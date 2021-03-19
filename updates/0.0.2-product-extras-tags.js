aa449dd19c3343c9dea8ff0abad26dd3eddd197a
nmasuki <masuki@geopoll.com>
2021-03-19 15:56:16 +0300

    Product option changes

---
+++ updates/0.0.2-product-extras-tags.js
@@ -0,0 +1,24 @@
+// Import
+var keystone = require('keystone');
+var Product = keystone.list('Product');
+
+exports = module.exports = function (done) {
+    var tags = ["cigars-and-ciggarrettes", "soft-drinks"];
+    
+    tags.forEach(tag => {
+        Product.search(tag, (err, products) => {
+            if(err) return console.warn("Error!!", err);
+                    
+            console.log(`Found ${products.length} products with tag '${tag}'`);
+            products.forEach(p => {
+                p.tags.push("extras"); 
+                p.save(err => {
+                    if(err) return console.warn("Error!!", err);
+                    console.log("Saved " + p.name);
+                });
+            });
+        });
+    });
+
+    done();
+};
