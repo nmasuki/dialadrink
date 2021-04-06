9bc7b6f966b9479d465fdaabf32c51b866a0feb9
nmasuki <masuki@geopoll.com>
2021-04-06 22:38:28 +0300

    Update

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var keystone = require('./app-init');
+var workers = require('./workers');
 console.log("Initializing keystone for " + process.env.NODE_ENV + " environment...");
 // Start Keystone to connect to your database and initialise the web server
+workers.start();
 keystone.start();
