231bbbf9d467cf37fab2b49afd9c1d9cf5ace57e
nmasuki <masuki@geopoll.com>
2021-04-07 11:55:19 +0300

    Update

---
+++ updates/0.0.3-sales.js
@@ -0,0 +1,39 @@
+var Sale = require('../helpers/LocalStorage').getInstance("sale");
+
+function getAppPaymentMethod(method){
+    //Cash,MPESA,PesaPal,COOP,Swipe On Delivery,Credit
+    var mapping = {
+        "Cash": ["Cash", "Cash on Delivery"],
+        "MPESA": ["MPESA", "MPESA on Delivery"],
+        "PesaPal": ["PesaPal"],
+        "COOP":["CyberSource"],
+    };
+
+    for(var i in mapping){
+        var match = mapping[i].find(x => x.toLowerCase() == method);
+        if(match)
+            return i;
+    }
+
+    return method.split(' ')[0];
+}
+
+module.exports = function (done) {
+	var sales = Sale.getAll().map(sale => {
+		if(sale.products){
+			sale.productIds = sale.products.map(p => p._id);
+			sale.products = null;
+		}
+
+		if(sale.client){
+			sale.clientId = sale.client._id;
+			sale.client = null;
+		}
+
+		sale.paymentMethod = getAppPaymentMethod(sale.paymentMethod);
+		return sale;
+	});
+
+	Sale.save(sales);
+	done();
+};
