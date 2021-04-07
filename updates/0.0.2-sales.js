79f4627f8825c139b059e32eb0b8685c9aca10a2
nmasuki <masuki@geopoll.com>
2021-04-07 11:35:33 +0300

    Update

--- updates/0.0.2-sales.js
+++ updates/0.0.2-sales.js
@@ -3,12 +3,12 @@ module.exports = function (done) {
 	var sales = Sale.getAll().map(sale => {
 		if(sale.products){
 			sale.productIds = sale.products.map(p => p._id);
-			delete sale.products;
+			sale.products = null;
 		}
 
 		if(sale.client){
 			sale.clientId = sale.client._id;
-			delete sale.client;
+			sale.client = null;
 		}
 
 		return sale;
