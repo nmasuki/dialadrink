eea302fb929c5197abcc80df47989a381189cd21
nmasuki <masuki@geopoll.com>
2021-04-15 23:43:40 +0300

    App API updates

--- workers/MakeSalesFromOrders.js
+++ workers/MakeSalesFromOrders.js
@@ -41,6 +41,7 @@ function getWork(next, done) {
                     _id: "sale-" + o._id,
                     dateOfSale: o.orderDate,
                     clientId: o.client.id,
+                    location: [o.delivery.address, o.delivery.building, o.delivery.houseNumber].filter(x => !!x).join(", "),
                     productIds: o.cart.selectMany(c => new Array(c.pieces || 1).join(',').split(',').map(x=> c.product.id)),
                     salePrice: o.total,
                     mode: "Online",
