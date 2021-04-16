02ba23bc5b3e79a0098ebeb098759194c114985f
nmasuki <masuki@geopoll.com>
2021-04-16 03:42:22 +0300

    App API updates

---
+++ updates/0.0.5-sales.js
@@ -0,0 +1,54 @@
+var Sale = require('../helpers/LocalStorage').getInstance("sale");
+
+function getAppPaymentMethod(method){
+	method = method || "";
+    //Cash,MPESA,PesaPal,COOP,Swipe On Delivery,Credit
+    var mapping = {
+        "Cash": ["Cash", "Cash on Delivery"],
+        "MPESA": ["MPESA", "MPESA on Delivery"],
+        "PesaPal": ["PesaPal"],
+        "COOP":["CyberSource"],
+    };
+
+    for(var i in mapping){
+        var match = mapping[i].find(x => x.toLowerCase() == method.toLowerCase());
+        if(match)
+            return i;
+    }
+
+    return method.split(' ')[0];
+}
+
+module.exports = function (done) {
+	var filter = {
+        orderDate: { $gt: new Date('2021-04-01') }
+    };
+
+    Order.model.find(filter)
+        .deepPopulate('client,cart.product.priceOptions.option')
+        .sort({orderDate: -1})
+        .exec(function (err, orders) {            
+            if(err)
+                return console.error("Error reading orders!", err || "Unknown");
+
+            var sales = orders.map(o => {
+                return {
+                    _id: "sale-" + o._id,
+                    dateOfSale: o.orderDate,
+                    clientId: o.client.id,
+                    location: [o.delivery.address, o.delivery.building, o.delivery.houseNumber].filter(x => !!x).join(", "),
+                    productIds: o.cart.selectMany(c => new Array(c.pieces || 1).join(',').split(',').map(x=> c.product.id)),
+                    salePrice: o.total,
+                    mode: "Online",
+                    paymentMethod: getAppPaymentMethod(o.paymentMethod)
+                };
+            });
+
+            if(sales.length)
+                console.log("Generating " + sales.length + " new online sale records as from " + filter.orderDate.$gt.toISOString());
+
+			Sale.save(sales);
+		});
+		
+	done();
+};
