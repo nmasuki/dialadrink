42b31767ceead38c10101fec3a6c019a1c1c5201
Nelson Masuki <nmasuki@mobileaccord.com>
2021-09-11 16:00:01 +0300

    Explicit popular

--- workers/ETLToLexirProducts.js
+++ workers/ETLToLexirProducts.js
@@ -26,7 +26,7 @@ function getWork(next, done) {
                 return console.error("Error reading products!", err || "Unknown");
 
 
-            var mapping = products.filter(p => p.defaultOption).map(p => {
+            var mapping = products.map(p => {
                 var map = {
                     _id: p._id,
                     name: p.name, //:{ type: String, required: true },
