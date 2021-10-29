e29451d21f60a9f9417a137d342d0a6346933133
Nelson Masuki <nmasuki@mobileaccord.com>
2021-10-30 00:45:12 +0300

    JS fixes

--- public/js/okhi.script.js
+++ public/js/okhi.script.js
@@ -1,31 +1,31 @@
 
-    
-$(document).ready(function(){    
+
+$(document).ready(function () {
     window.addressData = window.addressData || null;
 
-    $(document).on("load", "#lets-okhi-card iframe", function(){
+    $(document).on("load", "#lets-okhi-card iframe", function () {
         console.log(arguments);
     });
 
-    $(document).on("error", "#lets-okhi-card iframe", function(){
+    $(document).on("error", "#lets-okhi-card iframe", function () {
         console.error(arguments);
     });
 
-    var loadLocationCard = function(user){
+    var loadLocationCard = function (user) {
         var errorTimeOut;
         $('#lets-okhi').hide();
 
-        var handleOnSuccess = function (data){
+        var handleOnSuccess = function (data) {
             clearTimeout(errorTimeOut);
             window.addressData = data;
 
-            if (window.addressData){
+            if (window.addressData) {
                 app.cartUtil.loadCharges(window.addressData.location);
                 app.cartUtil.updateView();
             }
 
-            if(data && data.user && data.location){
-                $('#lets-okhi').hide(); 
+            if (data && data.user && data.location) {
+                $('#lets-okhi').hide();
 
                 if (data.user.firstName)
                     $("#firstName").val(data.user.firstName);
@@ -35,133 +35,148 @@ $(document).ready(function(){
                 $("[name=address]").val(data.location.title);
                 $("[name=building]").val(data.location.streetName);
                 $("[name=houseNumber]").val([data.location.propertyName, data.location.directions].join(', ').trim().trim(','));
-            }else{
+            } else {
                 $('#lets-okhi').show();
-                $("#lets-okhi-card").hide();          
+                $("#lets-okhi-card").hide();
             }
 
         };
-        
-        var handleOnError = function (data){
+
+        var handleOnError = function (data) {
             clearTimeout(errorTimeOut);
-            window.addressData = null;            
+            window.addressData = null;
             console.warn("Error Implimentation not done!", arguments);
 
-            $('#lets-okhi').animate({width:'toggle'}, 350);
+            $('#lets-okhi').animate({ width: 'toggle' }, 350);
             $("#lets-okhi-card").hide();
         };
 
         user = user || {
-            firstName:  $("#firstName").val(), // optional
+            firstName: $("#firstName").val(), // optional
             lastName: $("#lastName").val(), // optional
             phone: $("#phoneNumber").val(), // required
         };
-                
+
         if (!user.firstName) delete user.firstName;
         if (!user.lastName) delete user.lastName;
 
-        if(user.phone && user.phone.length >= 10){
-            errorTimeOut = setTimeout(function(){     
-                if(window.addressData) return;      
+        if (user.phone && user.phone.length >= 10) {
+            errorTimeOut = setTimeout(function () {
+                if (window.addressData) return;
+                if ($('#lets-okhi iframe').length) return;
+
                 $('#lets-okhi').hide();
-                $("#lets-okhi-card").parent().hide();            
+                $("#lets-okhi-card").parent().hide();
                 $("#addressInputs").slideDown();
-        
+
                 $(".alert-danger").find(".msg-text").html("<strong>Input Error while detecting your location! Please enter your address</strong>");
                 $(".alert-danger").slideDown();
-            }, 5000);            
-                
-            window.addressData = null;            
-            if (window.locationCard){
+            }, 5000);
+
+            window.addressData = null;
+            if (window.locationCard) {
                 window.locationCard.user = user;
             } else {
                 var element = document.getElementById("lets-okhi-card");
-                window.locationCard = new okhi.LocationCard({
-                    element: element, // required
-                    user: user, // required
-                    onSuccess: handleOnSuccess, // optional
-                    onError: handleOnError,// optional
-                    style: okhiStyle // optional
-                });
-            }     
-        }      
+                window.locationCard = new window.okhi.OkHiLocationCard(
+                    {
+                        user: new window.okhi.OkHiUser(user),
+                        style: okhiStyle // optional 
+                    },
+                    element,
+                    function (error, data) {
+                        if (error)
+                            return handleOnError(error);
+
+                        handleOnSuccess(data);
+                    });
+            }
+        }
     };
 
-    var loadLocationLookUp = function(user){
+    window.loadLocationLookUp = function (user) {
         var errorTimeOut;
         var handleOnSuccess = function (data) {
             clearTimeout(errorTimeOut);
             window.addressData = data;
 
-            $('#lets-okhi').animate({ width:'toggle' }, 350);
+            $('#lets-okhi').animate({ width: 'toggle' }, 350);
             $("#submitBtn").show();
-    
+
             $("[name=address]").val(data.location.title);
             $("[name=building]").val(data.location.streetName);
             $("[name=houseNumber]").val([data.location.propertyName, data.location.directions].join(', ').trim().trim(','));
-            
-            $("#lets-okhi").parent().show();              
-            loadLocationCard();           
+
+            $("#lets-okhi").parent().show();
+            loadLocationCard();
         };
-    
+
         var handleOnError = function (error) {
             clearTimeout(errorTimeOut);
             window.addressData = null;
-            
-            $('#lets-okhi').animate({width:'toggle'}, 350);
+
+            $('#lets-okhi').animate({ width: 'toggle' }, 350);
             $("#addressInputs").slideDown();
-    
+
             $(".alert-danger").find(".msg-text").html("<strong>Input Error while detecting your location!</strong> " + error);
             $(".alert-danger").slideDown();
         };
-    
+
         user = user || {
             phone: $("#phoneNumber").val(),
             firstName: $("#firstName").val(),
             lastName: $("#lastName").val(),
         };
 
-        if(user.phone && user.firstName && user.lastName){  
-            errorTimeOut = setTimeout(function(){   
-                window.addressData = null;         
-                $("#lets-okhi").parent().hide();            
+        if (user.phone && user.firstName && user.lastName) {
+            errorTimeOut = setTimeout(function () {
+                window.addressData = null;
+                if ($('#lets-okhi iframe').length) return;
+
+                $("#lets-okhi").parent().hide();
                 $("#addressInputs").slideDown();
-        
+
                 $(".alert-danger").find(".msg-text").html("<strong>Input Error while detecting your location! Please enter your address</strong>");
                 $(".alert-danger").slideDown();
             }, 5000);
-                  
-            var locationManager = new okhi.LocationManager({
+
+            var locationManager = new okhi.OkHiLocationManager({
                 user: user,
-                onSuccess: handleOnSuccess,
-                onError: handleOnError,
-                style: okhiStyle
+                config: okhiConfig,
+                style: okhiStyle,
+                mode: okhi.OkHiLocationManagerLaunchMode.select_location
             });
 
-            locationManager.launch({mode: 'select_location'});
-        }else{
+            locationManager.launch(function (error, data) {
+                if (error)
+                    return handleOnError(error);
+
+                // handle success data
+                handleOnSuccess(data);
+            });
+        } else {
             $(".alert-danger").find(".msg-text").html("<strong>Input Error!</strong> Invalid input found! Please fill in your <b>names</b> and <b>phone number</b>.")
             $(".alert-danger").slideDown();
         }
     };
 
-    if($("#phoneNumber").val())
+    if ($("#phoneNumber").val())
         loadLocationCard();
 
-    $(document).on("change", "#phoneNumber", function(e){
+    $(document).on("change", "#phoneNumber", function (e) {
         loadLocationCard({
-            firstName:  $("#firstName").val(), // optional
+            firstName: $("#firstName").val(), // optional
             lastName: $("#lastName").val(), // optional
             phone: $("#phoneNumber").val(), // required
         });
     });
 
-    $(document).on("click", '#lets-okhi', function(e){
+    $(document).on("click", '#lets-okhi', function (e) {
         e.preventDefault();
-            
+
         var form = $(this).parents("form");
         $(".alert-danger").hide();
-           
+
         loadLocationLookUp();
     });
 
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 121;
+var CACHE_VERSION = 122;
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
 function getCacheName(destination, inc) {
