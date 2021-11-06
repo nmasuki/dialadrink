a2a726ed271e24b450c60e673a15e3b44cdf16fc
Nelson Masuki <nmasuki@mobileaccord.com>
2021-11-06 21:11:42 +0300

    Auto uncreament sw.j version

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 131;
+var CACHE_VERSION = 134;
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
--- upgrade.sh
+++ upgrade.sh
@@ -1,23 +1,20 @@
-#rm .env?
-#rm upgrade.sh?
-
 cp .env /var/tmp/dialadrink-prod.env
 cp upgrade.sh /var/tmp/upgrade.sh
 
 git checkout .
 git pull -X theirs
 
-rm .env?
-rm upgrade.sh?
-
 cp /var/tmp/dialadrink-prod.env .env
 cp /var/tmp/upgrade.sh upgrade.sh
+./update-sw-version.sh
+
+rm .env?
+rm upgrade.sh?
 
 grunt build
 git add .
 git commit -m "Server updates"
 git push
-
 #pm2 reload dev --update-env --log-date-format 'DD-MM HH:mm:ss.SSS'
 pm2 reload main --update-env --log-date-format 'DD-MM HH:mm:ss.SSS'
 
