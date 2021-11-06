412012c20e90dd6a54ec42d9a94a020f86154f7d
Nelson Masuki <nmasuki@mobileaccord.com>
2021-11-06 21:44:53 +0300

    Server updates

--- upgrade.sh
+++ upgrade.sh
@@ -1,11 +1,16 @@
 cp .env /var/tmp/dialadrink-prod.env
 cp upgrade.sh /var/tmp/upgrade.sh
+
 git checkout master
 git pull -X theirs
+
 cp /var/tmp/dialadrink-prod.env .env
 cp /var/tmp/upgrade.sh upgrade.sh
+
 ./update-sw-version.sh
 grunt build
+
 git add . && git commit -m "Server updates" && git push
+
 pm2 reload main --update-env --log-date-format 'DD-MM HH:mm:ss.SSS'
 pm2 log
