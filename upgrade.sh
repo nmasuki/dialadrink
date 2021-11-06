e0f3542f7159d6d1fbce7c4da059ed686070f7ef
Nelson Masuki <nmasuki@mobileaccord.com>
2021-11-06 23:13:52 +0300

    Optimization  on filters

--- upgrade.sh
+++ upgrade.sh
@@ -1,11 +1,8 @@
-git stash clear
-git stash
 git pull -X theirs
 ./update-sw-version.sh
 #grunt build
 git add .
 git commit -m "Server updates"
 git push
-#git stash pop
 pm2 reload main --update-env --log-date-format 'DD-MM HH:mm:ss.SSS'
 pm2 log
