1429bcf5d9e53709f060458be68da07f1a69a8dc
Nelson <nmasuki@gmail.com>
2021-11-06 21:33:12 +0300

    Server updates

--- upgrade.sh
+++ upgrade.sh
@@ -1,21 +1,24 @@
-cp .env /var/tmp/dialadrink-prod.env
+cp .env /var/tmp/dialadrink-prod.env 
 cp upgrade.sh /var/tmp/upgrade.sh
 
-git checkout .
+git stash
 git pull -X theirs
 
-cp /var/tmp/dialadrink-prod.env .env
-cp /var/tmp/upgrade.sh upgrade.sh
+#cp /var/tmp/dialadrink-prod.env .env
+#cp /var/tmp/upgrade.sh upgrade.sh
 ./update-sw-version.sh
 
-rm .env?
-rm upgrade.sh?
-
 grunt build
-git add .
-git commit -m "Server updates"
-git push
+
+#Synct up git
+git add . && git commit -m "Server updates" && git push
+
+#Restore stashed
+git unstash
+
+#Reload service
 #pm2 reload dev --update-env --log-date-format 'DD-MM HH:mm:ss.SSS'
 pm2 reload main --update-env --log-date-format 'DD-MM HH:mm:ss.SSS'
 
+#Display logs
 pm2 log
