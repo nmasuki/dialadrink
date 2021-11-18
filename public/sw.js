b3c9eb8a0e5478be2c78fc80ef779faca8203b99
Nelson Masuki <nmasuki@mobileaccord.com>
2021-11-18 11:54:32 +0300

    nmasuki upgrade

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 169;
+var CACHE_VERSION = 170;
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,10 @@
 git pull -X theirs
-#./update-sw-version.sh
+
+oldnum=`head -n 1 public/sw.js | cut -d '=' -f2 | cut -d ';' -f1`
+newnum=`expr $oldnum + 1`
+echo  "$oldnum => $newnum"
+sed -i "1s/.*/var CACHE_VERSION = $newnum;/" public/sw.js
+
 #grunt build
 git add .
 git commit -m "$HOSTNAME upgrade"
