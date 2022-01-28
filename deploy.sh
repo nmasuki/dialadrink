166fa4b01062660de5308c30fe44bc79d7ec2722
Nelson Masuki <nmasuki@mobileaccord.com>
2022-01-28 12:29:27 +0300

    deloy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
-git pull -X theirs
-
+git pull -X theirs 
 oldnum=`head -n 1 public/sw.js | cut -d '=' -f2 | cut -d ';' -f1`
 newnum=`expr $oldnum + 1`
 echo  "$oldnum => $newnum"
 sed -i "1s/.*/var CACHE_VERSION = $newnum;/" public/sw.js
-
 grunt build
 git add .
 git commit -m "$HOSTNAME upgrade"
