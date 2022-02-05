8ed16e3cbbfc6deddc067a18a59ca639f416c23e
Nelson Masuki <nmasuki@mobileaccord.com>
2022-02-05 10:59:58 +0300

    Conditions on deploy

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,7 @@ if [ $RESULT -eq 0 ]; then
     newnum=`expr $oldnum + 1`
     echo  "$oldnum => $newnum"
     sed -i "1s/.*/var CACHE_VERSION = $newnum;/" public/sw.js
-    grunt build
-    git add .
-    git commit -m "$HOSTNAME upgrade"
-    git push
-    pm2 reload main --update-env --log-date-format 'DD-MM HH:mm:ss.SSS'
-    pm2 log
+    grunt build && git add . && git commit -m "$HOSTNAME upgrade" && git push
+    pm2 reload main --update-env --log-date-format 'DD-MM HH:mm:ss.SSS' && pm2 log
 fi
 
