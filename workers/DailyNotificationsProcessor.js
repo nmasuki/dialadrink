48bd66fc458e3a63e5498fdf21d88c858bfc7f22
Nelson Masuki <nmasuki@mobileaccord.com>
2022-03-08 13:33:42 +0300

    Daily notifications

--- workers/DailyNotificationsProcessor.js
+++ workers/DailyNotificationsProcessor.js
@@ -11,18 +11,18 @@ const messageTemplate = {
         "Happy {dayOfWeek} {firstName} and a lovely evening!",
     ],
     dailyMessage: [
-        /*Sunday*/  [],
-        /*Monday*/  [],
-        /*Tuesday*/ [],
+        /*Sunday*/[],
+        /*Monday*/[],
+        /*Tuesday*/[],
         /*Wednesday*/[],
         /*Thursday*/[],
-        /*Friday*/  [],
+        /*Friday*/[],
         /*Sartuday*/[],
     ],
-    message: [        
+    message: [
         "We have some great offers on {favoriteDrink} for you!",
         "It's Another wonderful {dayOfWeek} to enjoy {favoriteDrink} with friends.",
-        "Are you in the mood for a cold {favoriteDrink}?",
+        "Are you in the mood for a cold glass of {favoriteDrink}?",
         "It is a terrific day for a {favoriteDrink}.",
         "We have some crazy offers this {dayOfWeek}!",
         "It's a wonderful {dayOfWeek} and we have great offers on {favoriteDrink} just for you.",
@@ -30,7 +30,7 @@ const messageTemplate = {
         "{dayOfWeek} is the best day for a cold {favoriteDrink}.",
         "We know {dayOfWeek}'s are long. Enjoy a bottle of {favoriteDrink} with friends to relax."
     ],
-    outro:[
+    outro: [
         "Order a bottle right now!",
         "Order now! We will deliver with 30 mins",
         "Order now and enjoy free delivery within Nairobi",
@@ -39,18 +39,18 @@ const messageTemplate = {
         "Have it delivered right to your door steps",
     ],
 
-    get: function(obj, section){
-        if(!messageTemplate[section]) section = 'message';
+    get: function (obj, section) {
+        if (!messageTemplate[section]) section = 'message';
         return messageTemplate[section][randomInt(0, messageTemplate[section].length - 1)].format(obj).replace(/\s(\W)\s/g, '$1 ')
     },
 
-    getMessage: function(obj){
+    getMessage: function (obj) {
         var parts = ['intro', 'message', 'outro'];
         var message = '';
 
-        for(var i of parts)
-            if(messageTemplate.hasOwnProperty(i))
-                message += (message? ' ': '') + messageTemplate[i][randomInt(0, messageTemplate[i].length - 1)].format(obj).replace(/\s(\W)\s/g, '$1 ');
+        for (var i of parts)
+            if (messageTemplate.hasOwnProperty(i))
+                message += (message ? ' ' : '') + messageTemplate[i][randomInt(0, messageTemplate[i].length - 1)].format(obj).replace(/\s(\W)\s/g, '$1 ');
 
         return message.trim();
     }
@@ -97,18 +97,29 @@ function doWork(err, clients, next) {
         if (clients.length)
             console.log(clients.length + " client to send daily notifications to..");
 
-        async function popNext(){
-            if(clients.length){
-                var client = clients.pop();
-                await createNotification(client);
+        var promise = new Promise((resolve, reject) => {
+            var proseses = [];
+            (function popNext() {
+                if (clients.length) {
+                    var client = clients.pop();
+                    return createNotification(client).then(() => {
+                        if (clients.length)
+                            return popNext();
 
-                if(client.length) 
-                    return await popNext();
-            }
-            console.log("Created all notifications!")
-        };
+                        proseses.push(client);
+                        console.log("Prosses " + proseses.length);
+                    });
+                }
 
-        return popNext().then(() => typeof next == "function" ? next() : null);
+                console.log(`Created all ${proseses.length} notifications!`);
+                resolve(proseses)
+            })();
+        });
+
+        return promise.then(() => {
+            if (typeof next == "function")
+                next();
+        });
     } else {
         if (typeof next == "function")
             next();
@@ -117,7 +128,7 @@ function doWork(err, clients, next) {
     }
 }
 
-function randomInt(min, max) {    
+function randomInt(min, max) {
     return min + Math.round(Math.random() * (max - min));
 }
 
@@ -159,13 +170,13 @@ async function createNotification(client) {
         }
     } else {
         n.message.body = `DIALADRINK: ${n.message.body}.`;
-        if(!n.message.body.contains('http'))
+        if (!n.message.body.contains('http'))
             n.message.body += '. http://bit.ly/2TCl4MI';
     }
-    
+
     console.log(`'${n.type.toUpperCase()}' to ${client.name}: ${n.message.body}`);
     if (process.env.NODE_ENV == "production")
-        return await n.save();    
+        return await n.save();
 }
 
 var lastRun = new Date().addDays(-2);
