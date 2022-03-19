c2d3d6708a3160f916d683ff49dbba65047e0884
Nelson Masuki <nmasuki@mobileaccord.com>
2022-03-19 21:52:13 +0300

    Updates

--- routes/apis/productsv2.js
+++ routes/apis/productsv2.js
@@ -8,36 +8,46 @@ var router = keystone.express.Router();
 
 router.get("/", function (req, res) {
     var filter = {};
-
-    if(req.query.id)
-        filter._id = typeof req.query.id == "string"? req.query.id: {"$in": req.query.id.map(id => id)};
-    
-    var page = parseInt(req.query.page || 1);
-    var pageSize = parseInt(req.query.pageSize || 1000);
-    var start = (page - 1) * pageSize;
-    
+    var ids = req.query.id || req.query.ids;
     var query = req.query.query || "";
-    console.log("Looking up products.. " + query, "page:", page, "pageSize:", pageSize, "skip:", start);
-    Product.search(query, function (err, products) {
-        var json = {
-            response: "error",
-            message: ""
-        };
+    
+    if(ids && Array.isArray(ids))
+        filter._id = {"$in": ids.map(id => id)};
+    else 
+        filter = filters.luceneToMongo(ids || query);
 
+    
+    var orderBy = req.query.sort || req.query.orderBy || "popularityRatio DESC";
+    var page = parseInt(req.query.page || 1);
+    var pageSize = parseInt(req.query.pageSize || 1500);
+    var skip = (page - 1) * pageSize;
+    var sort = filters.orderByToSortObj(orderBy);
+    
+    console.log("Running filter on products:" + JSON.stringify(filter));
+    var json = { response: "error", message: "", count: 0, data: [] };
+
+    try{
+        var products = Product.model.find(filter)
+            .sort(sort).skip(skip).limit(pageSize)
+            .populate('brand').populate('category').populate('ratings')
+            .deepPopulate("subCategory.category,priceOptions.option")
+            .exec();
+
+        if (products && products.length) {
+            json.response = "success";
+            json.count = products.length;
+            json.data = products.map(d => d.toAppObject());
+        } else {
+            json.response = "success";
+            json.message = "No record matching the query";
+        }
+    } catch(err){
         if (err)
             json.message = "Error fetching drinks! " + err;
-        else {
-            json.response = "success";
-            
-            json.data = products.orderBy(p => p.publishedDate)
-                .slice(start, start + pageSize)
-                .map(d => Object.assign({ options: d.options }, d.toAppObject()));
+    }
 
-            console.log("Found " + json.data.length);
-        }
 
-        res.send(json);
-    });
+    res.send(json);
 });
 
 router.get("/:query", async function (req, res, next) {
